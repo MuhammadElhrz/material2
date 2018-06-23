d81826b915bdc9582bfdd69f2c1034718e825c88
crisbeto <crisbeto@abv.bg>
2018-06-23 19:24:30 +0200

    fix(checkbox): model value not updated when using toggle method
    Along the same lines as #11812. The checkbox doesn't update its `ControlValueAccessor` value when it is toggled via the `toggle` method.

--- src/lib/checkbox/checkbox.ts
+++ src/lib/checkbox/checkbox.ts
@@ -353,6 +353,7 @@ export class MatCheckbox extends _MatCheckboxMixinBase implements ControlValueAc
   /** Toggles the `checked` state of the checkbox. */
   toggle(): void {
     this.checked = !this.checked;
+    this._controlValueAccessorChangeFn(this.checked);
   }
 
   /**
