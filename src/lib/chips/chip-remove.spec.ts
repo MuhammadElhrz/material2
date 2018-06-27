64a06d65357c6b0ad84ef201abc0a8b16612a8c1
crisbeto <crisbeto@abv.bg>
2018-06-27 19:23:02 +0200

    fix(chips): remove button not accessible via keyboard
    * Fixes the user not being able to reach the chip remove button using the keyboard. Now the button will become tabbable once the chip around it is focused.
    * Fixes the remove button not doing anything on enter key presses.
    
    Fixes #11896.

--- src/lib/chips/chip-remove.spec.ts
+++ src/lib/chips/chip-remove.spec.ts
@@ -2,6 +2,8 @@ import {Component, DebugElement} from '@angular/core';
 import {By} from '@angular/platform-browser';
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 import {MatChip, MatChipsModule} from './index';
+import {dispatchFakeEvent, dispatchKeyboardEvent} from '@angular/cdk/testing';
+import {ENTER} from '@angular/cdk/keycodes';
 
 describe('Chip Remove', () => {
   let fixture: ComponentFixture<TestChip>;
@@ -30,34 +32,79 @@ describe('Chip Remove', () => {
   }));
 
   describe('basic behavior', () => {
-    it('should applies the `mat-chip-remove` CSS class', () => {
-      let hrefElement = chipNativeElement.querySelector('a')!;
+    it('should apply the `mat-chip-remove` CSS class', () => {
+      const hrefElement = chipNativeElement.querySelector('a')!;
 
       expect(hrefElement.classList).toContain('mat-chip-remove');
     });
 
-    it('should emits (removed) on click', () => {
-      let hrefElement = chipNativeElement.querySelector('a')!;
+    it('should emit (removed) on click', () => {
+      const hrefElement = chipNativeElement.querySelector('a')!;
+
+      testChip.removable = true;
+      fixture.detectChanges();
+      hrefElement.click();
+
+      expect(testChip.didRemove).toHaveBeenCalled();
+    });
+
+    it('should become focusable when the chip is focused', () => {
+      const removeElement = chipNativeElement.querySelector('a')!;
+
+      expect(removeElement.getAttribute('tabindex')).toBe('-1');
+
+      dispatchFakeEvent(chipNativeElement, 'focus');
+      fixture.detectChanges();
+
+      expect(removeElement.getAttribute('tabindex')).toBe('0');
+
+      dispatchFakeEvent(chipNativeElement, 'blur');
+      fixture.detectChanges();
+
+      expect(removeElement.getAttribute('tabindex')).toBe('-1');
+    });
+
+    it('should not overwrite the tabindex if it was set by the consumer', () => {
+      const removeElement = chipNativeElement.querySelector('a')!;
+
+      fixture.componentInstance.chipRemoveTabindex = 3;
+      fixture.detectChanges();
+
+      expect(removeElement.getAttribute('tabindex')).toBe('3');
+
+      dispatchFakeEvent(chipNativeElement, 'focus');
+      fixture.detectChanges();
+
+      expect(removeElement.getAttribute('tabindex')).toBe('3');
+    });
+
+    it('should remove the chip when pressing enter', () => {
+      const hrefElement = chipNativeElement.querySelector('a')!;
 
       testChip.removable = true;
       fixture.detectChanges();
 
-      spyOn(testChip, 'didRemove');
-
-      hrefElement.click();
+      const event = dispatchKeyboardEvent(hrefElement, 'keydown', ENTER);
+      fixture.detectChanges();
 
       expect(testChip.didRemove).toHaveBeenCalled();
+      expect(event.defaultPrevented).toBe(true);
     });
   });
 });
 
 @Component({
   template: `
-    <mat-chip [removable]="removable" (removed)="didRemove()"><a matChipRemove></a></mat-chip>
+    <mat-chip
+      [removable]="removable"
+      (removed)="didRemove()">
+
+      <a [tabIndex]="chipRemoveTabindex" matChipRemove></a>
+  </mat-chip>
   `
 })
 class TestChip {
+  chipRemoveTabindex: number;
   removable: boolean;
-
-  didRemove() {}
+  didRemove = jasmine.createSpy('remove spy');
 }
